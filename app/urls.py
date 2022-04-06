d42dc26c8ef97e0dcbaac18ea9ed63122aa8d7a4
Zhayns <aazc2016@gmail.com>
2022-04-06 19:28:09 -0400

    Agregar productos django

--- app/urls.py
+++ app/urls.py
@@ -1,6 +1,10 @@
 from django.urls import path
-from .views import home,sobrenosotros
+from .views import home, sobreNosotros, productos, carrito,contacto
+
 urlpatterns = [
-    path('', home,name="home"),
-    path('sobrenosotros/', sobrenosotros,name="sobrenosotros"),
+    path('', home, name="home"),
+    path('sobreNosotros/', sobreNosotros, name="sobreNosotros"),
+    path('productos/', productos, name="productos"),
+    path('carrito/', carrito, name="carrito"),
+    path('contacto/',contacto , name="contacto")
 ]
--- app/views.py
+++ app/views.py
@@ -1,9 +1,37 @@
+from urllib.request import Request
 from django.shortcuts import render
-
+from .models import Producto
+from .forms import ContactoForms
 # Create your views here.
 
-def home (request ):
+def home(request):
     return render(request, 'app/home.html')
 
-def sobrenosotros (request ):
-    return render(request, 'app/SobreNosotros.html')
+def sobreNosotros(request):
+    return render(request, 'app/sobreNosotros.html')    
+
+def contacto(request):
+    data = {
+        'form': ContactoForms()
+    }
+
+    if request.method == 'POST':
+        formulario = ContactoForms(data=request.POST)
+        if formulario.is_valid():
+            formulario.save()
+            data["mensaje"]= "contacto guardado"
+        else:
+            data["form"] = formulario
+
+    return render(request, 'app/contacto.html',data)
+
+def productos(request):
+
+    productos = Producto.objects.all()
+    data = {
+        'productos' : productos
+    }
+    return render(request, 'app/productos.html',data) 
+
+def carrito(request):
+    return render(request, 'app/carrito.html')
